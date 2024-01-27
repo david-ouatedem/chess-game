abbca790f4f6514890436681ec0e66afa2df9c3c
davdev14 <davdev1400@gmail.com>
2024-01-27 20:13:07 +0100

    Added event listener to handle check which peice was clicked

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,33 @@ class Pawn {
   create() {
     this.peice.src = "/src/assets/peices/black/black-pawn.svg";
     this.peice.alt = "pawn";
+    this.peice.classList.add("piece");
     this.peice.width = 24;
     a1?.appendChild(this.peice);
   }
+  handleShowMoves() {
+    if (a1 === null) return;
+    document.body.addEventListener("click", (event) => {
+      const clickedPeice = event.target as HTMLElement;
+      if (
+        clickedPeice.tagName === "IMG" &&
+        clickedPeice.classList.contains("piece")
+      ) {
+        const imageElement = clickedPeice as HTMLImageElement;
+        if (imageElement.alt !== "pawn") return;
+        console.log("peice pawn", imageElement);
+      }
+    });
+  }
   move() {
-    a2?.appendChild(this.peice);
+    if (a2 === null) return;
+    a2.onclick = () => {
+      if (a2 === null) return;
+      a2.appendChild(this.peice);
+    };
   }
 }
 const pawn1 = new Pawn();
 pawn1.create();
+pawn1.handleShowMoves();
 pawn1.move();
