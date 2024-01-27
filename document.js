d9e9f3ca2bbe65da088f0a2d9d342744683802e1
davdev14 <davdev1400@gmail.com>
2024-01-27 09:08:01 +0100

    migration to vite

--- document.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.a2 = exports.a1 = void 0;
-exports.a1 = document.getElementById("a1");
-exports.a2 = document.getElementById("a2");
--- document.ts
+++
@@ -1,2 +0,0 @@
-export const a1: HTMLElement | null = document.getElementById("a1");
-export const a2: HTMLElement | null = document.getElementById("a2");
